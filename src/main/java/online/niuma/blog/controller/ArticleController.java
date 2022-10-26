55ee42151aaa675ba49c7aec186be413c6f81770
NiuMa <2493378918@qq.com>
2022-10-26 23:52:29 +0800

    add ArticleController class

---
+++ src/main/java/online/niuma/blog/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package online.niuma.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 一颗蛋50斤
+ * IntelliJ IDEA
+ */
+@RestController
+public class ArticleController {
+}
