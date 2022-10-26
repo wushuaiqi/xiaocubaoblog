7a3fa7a798c149f6cb9d3aac3465a02c3fe5cb02
NiuMa <2493378918@qq.com>
2022-10-26 14:07:35 +0800

    新增文章实体类

---
+++ src/main/java/online/niuma/blog/pojo/Articles.java
@@ -0,0 +1,29 @@
+package online.niuma.blog.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author 一颗蛋50斤
+ * IntelliJ IDEA
+ */
+@Data
+public class Articles {
+
+    private Long articleId;
+
+    private String articleTitle;
+
+    private String articleSketch;
+
+    private Long userId;
+
+    private Long contentId;
+
+    private int deleted;
+
+    private Date createTime;
+
+    private Date updateTime;
+}
